a1f5820ab0603371d4a7a4b99cf02b6a706764cf
Yaşar Demir <yasarbeyler73@gmail.com>
2026-05-28 10:38:48 +0300

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -5,6 +5,7 @@ export const InputSchema = z.object({
     // Search parameters
     query: z.string().optional(),
     searchUrl: z.string().optional(),
+    searchUrls: z.array(z.string().url()).optional(),
     categoryUrl: z.string().url().optional(),
     shopUrl: z.string().url().optional(),
     productUrls: z.array(z.string().url()).optional(),
